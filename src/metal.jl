829a67ba9527aac768f0af28c64c08a48de859f9
Jesper Stemann Andersen <jesper@sait.dk>
2024-11-18 20:42:55 +0100

    Added a few device_info functions

--- src/metal.jl
+++ src/metal.jl
@@ -1,3 +1,8 @@
+function metal_device_info()
+    device_info = Wrapper.mlx_metal_device_info()
+    return device_info
+end
+
 function metal_is_available()
     result = Ref(false)
     Wrapper.mlx_metal_is_available(result)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 @testset verbose = true "MLX" begin
     include(joinpath(@__DIR__, "device_tests.jl"))
     include(joinpath(@__DIR__, "error_handling_tests.jl"))
+    include(joinpath(@__DIR__, "metal_tests.jl"))
     if !Sys.iswindows() # Windows is hanging
         include(joinpath(@__DIR__, "stream_tests.jl"))
     end
