faec22ce750c06df152221bac680a84d6fbd5a47
Jesper Stemann Andersen <jesper@sait.dk>
2024-11-18 20:42:55 +0100

    Added a few device_info functions

--- src/MLX.jl
+++ src/MLX.jl
@@ -2,4 +2,7 @@ module MLX
 
 include(joinpath(@__DIR__, "Wrapper.jl"))
 
+include(joinpath(@__DIR__, "device.jl"))
+include(joinpath(@__DIR__, "metal.jl"))
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using Test
 
 @testset "MLX" begin
+    include(joinpath(@__DIR__, "device_tests.jl"))
+    include(joinpath(@__DIR__, "metal_tests.jl"))
 end
