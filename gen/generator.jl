a4623401d12c3366c70e9c3190377f4bcb04ed7b
Jesper Stemann Andersen <jesper@sait.dk>
2024-11-18 20:20:17 +0100

    Fixup wrapper generator

--- gen/generator.jl
+++ gen/generator.jl
@@ -9,6 +9,7 @@ options = load_options(joinpath(@__DIR__, "generator.toml"))
 
 args = get_default_args()
 push!(args, "-I$include_dir")
+push!(args, "-I$(joinpath(MLX_C_jll.artifact_dir, "include", "mlx"))")
 
 headers = [
     joinpath(include_dir, header) for
