5b8ecd6db369532dd9b35a725d20b6cd00dfedbf
Jesper Stemann Andersen <jesper@sait.dk>
2024-12-14 12:09:04 +0100

    Added register_error_handler
    Also, fixed formatting.

--- src/MLX.jl
+++ src/MLX.jl
@@ -7,8 +7,8 @@ include(joinpath(@__DIR__, "Wrapper.jl"))
 include(joinpath(@__DIR__, "error_handling.jl"))
 
 function __init__()
-    error_handler_c = @cfunction(error_handler, Cvoid, (Ptr{Cchar},));
-    Wrapper.mlx_set_error_handler(error_handler_c, C_NULL, C_NULL)
+    register_error_handler()
+    return nothing
 end
 
 end
--- src/error_handling.jl
+++ src/error_handling.jl
@@ -4,5 +4,11 @@ end
 
 function error_handler(s::Ptr{Cchar})::Cvoid
     throw(MLXException(unsafe_string(s)))
-    return
+    return nothing
+end
+
+function register_error_handler()
+    error_handler_c = @cfunction(error_handler, Cvoid, (Ptr{Cchar},))
+    Wrapper.mlx_set_error_handler(error_handler_c, C_NULL, C_NULL)
+    return nothing
 end
