f738a96d2d09b32016cdabf47e52c76fa9ec0ab7
Jesper Stemann Andersen <jesper@sait.dk>
2024-12-14 15:12:53 +0100

    Added MLXArray
    Also, added MLXVector, MLXMatrix, and MLXVecOrMat.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 
 @testset verbose = true "MLX" begin
+    include(joinpath(@__DIR__, "array_tests.jl"))
     include(joinpath(@__DIR__, "device_tests.jl"))
     include(joinpath(@__DIR__, "error_handling_tests.jl"))
     if !Sys.iswindows() # Windows is hanging
