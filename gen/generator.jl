7383e4b719d8095510e945fde73a0b3ed4fd129a
Jesper Stemann Andersen <jesper@sait.dk>
2024-12-15 19:08:36 +0100

    Wrapper: Added Float16 and BFloat16 methods

--- gen/generator.jl
+++ gen/generator.jl
@@ -10,6 +10,10 @@ options = load_options(joinpath(@__DIR__, "generator.toml"))
 args = get_default_args()
 push!(args, "-I$(joinpath(MLX_C_jll.artifact_dir, "include"))")
 
+# Ensure Float16 and BFloat16 methods are generated - even on platforms without support
+push!(args, "-DHAS_FLOAT16")
+push!(args, "-DHAS_BFLOAT16")
+
 headers = [
     joinpath(include_dir, header) for
     header in readdir(include_dir) if endswith(header, ".h")
