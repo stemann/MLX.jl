eba8a4e8763699aa8f5525a060d5b7d1637d6c8f
Jesper Stemann Andersen <jesper@sait.dk>
2024-12-19 16:38:54 +0100

    Fixup

--- test/array_tests.jl
+++ test/array_tests.jl
@@ -2,6 +2,7 @@ using MLX
 using Test
 
 @testset "array" begin
+    @test IndexStyle(MLXArray) == IndexLinear()
     element_types = (
         Bool,
         UInt8,
@@ -12,20 +13,38 @@ using Test
         Int16,
         Int32,
         Int64,
-        Float16,
+        # TODO Float16, 
         Float32,
+        # TODO Core.BFloat16
         ComplexF32,
     )
     for T in element_types, N in (0, 1, 2, 3)
         @testset "Array{$T, $N}" begin
             array = repeat([T(1)], N)
-            mlx_array = MLXArray(array)
+            if array isa AbstractVector
+                mlx_array = MLXVector(array)
+            elseif array isa AbstractMatrix
+                mlx_array = MLXMatrix(array)
+            else
+                mlx_array = MLXArray(array)
+            end
             @test eltype(mlx_array) == T
             @test length(mlx_array) == length(array)
             @test ndims(mlx_array) == ndims(array)
             @test size(mlx_array) == size(array)
             @test strides(mlx_array) == strides(array)
             @test Base.elsize(mlx_array) == Base.elsize(array)
+
+            if N > 0
+                @test getindex(mlx_array, 1) == T(1)
+                array[1] = T(1)
+                @test setindex!(mlx_array, T(1), 1) == array
+            end
+
+            @test unsafe_wrap(mlx_array) == array
         end
+    end
+    for T in [Float64]
+        @test_throws ArgumentError convert(MLX.Wrapper.mlx_dtype, T)
     end
 end
