58df60114c8a9080fe8f177b45ac763e4a36daad
Jesper Stemann Andersen <jesper@sait.dk>
2025-02-01 10:07:24 +0100

    WIP

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,5 @@ using Test
     include(joinpath(@__DIR__, "array_tests.jl"))
     include(joinpath(@__DIR__, "device_tests.jl"))
     include(joinpath(@__DIR__, "error_handling_tests.jl"))
-    if !Sys.iswindows() # Windows is hanging
-        include(joinpath(@__DIR__, "stream_tests.jl"))
-    end
+    include(joinpath(@__DIR__, "stream_tests.jl"))
 end
