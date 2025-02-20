10b9e887c56b85c4211c2745ad0f1b25c97f7087
Jesper Stemann Andersen <jesper@sait.dk>
2025-02-21 00:50:36 +0100

    Added env. var. for selecting BLAS to use for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,39 @@
+using LinearAlgebra
+using MLX
 using Test
 
-@testset verbose = true "MLX" begin
-    include(joinpath(@__DIR__, "array_tests.jl"))
-    include(joinpath(@__DIR__, "device_tests.jl"))
-    include(joinpath(@__DIR__, "error_handling_tests.jl"))
-    include(joinpath(@__DIR__, "stream_tests.jl"))
+function run_tests()
+    blas_config = BLAS.get_config()
+    @info "Running tests using BLAS config: $blas_config"
+    @testset verbose = true "MLX" begin
+        include(joinpath(@__DIR__, "array_tests.jl"))
+        include(joinpath(@__DIR__, "device_tests.jl"))
+        include(joinpath(@__DIR__, "error_handling_tests.jl"))
+        include(joinpath(@__DIR__, "stream_tests.jl"))
+    end
+end
+
+blas_defined = haskey(ENV, "BLAS")
+if blas_defined
+    blas = ENV["BLAS"]
+    @info "Ensuring selected BLAS is loaded: $blas"
+    if blas == "AppleAccelerate"
+        using AppleAccelerate
+    elseif blas == "MKL"
+        using MKL
+    elseif blas == "OpenBLAS"
+        # OpenBLAS should already be loaded
+    end
+end
+
+run_tests()
+
+if !blas_defined
+    if Sys.ARCH == :x86_64 && (Sys.islinux() || Sys.iswindows())
+        using MKL
+        run_tests()
+    elseif Sys.isapple()
+        using AppleAccelerate
+        run_tests()
+    end
 end
