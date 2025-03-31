dedf6bc7e7b2abfd021f8a22d4956fa7299912a9
Jesper Stemann Andersen <jesper@sait.dk>
2025-03-31 11:53:29 +0200

    Added unary ops
    * Workaround for sign on CPU for unsigned
    * Using Julia 1.10 compatible `ceil`, `floor`, and `round`: `ceil(T, x)` -> `T(ceil(x))`

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
     include(joinpath(@__DIR__, "array_tests.jl"))
     include(joinpath(@__DIR__, "device_tests.jl"))
     include(joinpath(@__DIR__, "error_handling_tests.jl"))
+    include(joinpath(@__DIR__, "ops_tests.jl"))
     include(joinpath(@__DIR__, "stream_tests.jl"))
 end
