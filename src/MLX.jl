a4e8c818b6a599bd6110b64f32f1a624a45061fa
Jesper Stemann Andersen <jesper@sait.dk>
2025-03-31 11:53:29 +0200

    Added unary ops
    * Workaround for sign on CPU for unsigned
    * Using Julia 1.10 compatible: ceil(T, x) -> T(ceil(x)), floor(T, x) -> T(floor(x)) and round(T, x) -> T(round(x))

--- src/MLX.jl
+++ src/MLX.jl
@@ -10,11 +10,15 @@ export MLXArray, MLXException, MLXMatrix, MLXVecOrMat, MLXVector
 
 include(joinpath(@__DIR__, "Wrapper.jl"))
 
+include(joinpath(@__DIR__, "Private.jl"))
+
 include(joinpath(@__DIR__, "array.jl"))
 include(joinpath(@__DIR__, "device.jl"))
 include(joinpath(@__DIR__, "error_handling.jl"))
 include(joinpath(@__DIR__, "metal.jl"))
 include(joinpath(@__DIR__, "stream.jl"))
+
+include(joinpath(@__DIR__, "ops.jl"))
 
 function __init__()
     register_error_handler()
---
+++ test/ops_tests.jl
@@ -0,0 +1,65 @@
+using MLX
+using Random
+using Test
+
+@testset "ops" begin
+    Random.seed!(42)
+
+    element_types = (
+        Bool,
+        UInt8,
+        UInt16,
+        UInt32,
+        UInt64,
+        Int8,
+        Int16,
+        Int32,
+        Int64,
+        # TODO Float16, 
+        Float32,
+        # TODO Core.BFloat16
+        ComplexF32,
+    )
+    array_sizes = [
+        # (), # TODO: Excluded broadcasting over 0-dimensional Array: Yields scalar result
+        (1,),
+        (2,),
+        (1, 1),
+        (2, 1),
+        (2, 2),
+        (1, 1, 1),
+    ]
+    for (fn, fn_def) in MLX.Private.get_unary_scalar_ops()
+        @testset "$fn" begin
+            for T in element_types, array_size in array_sizes
+                N = length(array_size)
+                if !(T <: fn_def.TIn)
+                    continue
+                end
+                @testset "$fn.(::$MLXArray{$T, $N}), $array_size" begin
+                    array = rand(T, array_size)
+                    array = fn_def.normalize(array, T)
+                    if N > 2 || N == 0
+                        mlx_array = MLXArray(array)
+                    elseif N > 1
+                        mlx_array = MLXMatrix(array)
+                    else
+                        mlx_array = MLXVector(array)
+                    end
+                    TOut = fn_def.output_type(T)
+                    if TOut == Float32 # TODO fn.(array) may return a Float64 array for a Float32 array
+                        expected = @eval $TOut.($fn.($array))
+                    else
+                        expected = @eval $fn.($array)
+                    end
+                    actual = @eval $fn.($mlx_array)
+                    if TOut <: Integer
+                        @test actual == expected
+                    else
+                        @test actual ≈ expected
+                    end
+                end
+            end
+        end
+    end
+end
