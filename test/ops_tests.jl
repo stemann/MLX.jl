6301d86c7e70b8ac07b7b33ac8d69a7abb7232bf
Jesper Stemann Andersen <jesper@sait.dk>
2025-03-31 15:46:30 +0200

    Included test of ops on GPU if metal_is_available

--- test/ops_tests.jl
+++ test/ops_tests.jl
@@ -1,3 +1,8 @@
+@static if VERSION < v"1.11"
+    using ScopedValues
+else
+    using Base.ScopedValues
+end
 using MLX
 using Random
 using Test
@@ -5,6 +10,10 @@ using Test
 @testset "ops" begin
     Random.seed!(42)
 
+    device_types = [MLX.DeviceTypeCPU]
+    if MLX.metal_is_available()
+        push!(device_types, MLX.DeviceTypeGPU)
+    end
     element_types = (
         Bool,
         UInt8,
@@ -31,32 +40,38 @@ using Test
     ]
     for (fn, fn_def) in MLX.Private.get_unary_scalar_ops()
         @testset "$fn" begin
-            for T in element_types, array_size in array_sizes
-                N = length(array_size)
-                if !(T <: fn_def.TIn)
-                    continue
-                end
-                @testset "$fn.(::$MLXArray{$T, $N}), $array_size" begin
-                    array = rand(T, array_size)
-                    array = fn_def.normalize(array, T)
-                    if N > 2 || N == 0
-                        mlx_array = MLXArray(array)
-                    elseif N > 1
-                        mlx_array = MLXMatrix(array)
-                    else
-                        mlx_array = MLXVector(array)
-                    end
-                    TOut = fn_def.output_type(T)
-                    if TOut == Float32 # TODO fn.(array) may return a Float64 array for a Float32 array
-                        expected = @eval $TOut.($fn.($array))
-                    else
-                        expected = @eval $fn.($array)
-                    end
-                    actual = @eval $fn.($mlx_array)
-                    if TOut <: Integer
-                        @test actual == expected
-                    else
-                        @test actual ≈ expected
+            for device_type in device_types
+                @testset "$fn, with $device_type" begin
+                    with(MLX.device => MLX.Device(; device_type)) do
+                        for T in element_types, array_size in array_sizes
+                            N = length(array_size)
+                            if !(T <: fn_def.TIn)
+                                continue
+                            end
+                            @testset "$fn.(::$MLXArray{$T, $N}), $array_size, with $device_type" begin
+                                array = rand(T, array_size)
+                                array = fn_def.normalize(array, T)
+                                if N > 2 || N == 0
+                                    mlx_array = MLXArray(array)
+                                elseif N > 1
+                                    mlx_array = MLXMatrix(array)
+                                else
+                                    mlx_array = MLXVector(array)
+                                end
+                                TOut = fn_def.output_type(T)
+                                if TOut == Float32 # TODO fn.(array) may return a Float64 array for a Float32 array
+                                    expected = @eval $TOut.($fn.($array))
+                                else
+                                    expected = @eval $fn.($array)
+                                end
+                                actual = @eval $fn.($mlx_array)
+                                if TOut <: Integer
+                                    @test actual == expected
+                                else
+                                    @test actual ≈ expected
+                                end
+                            end
+                        end
                     end
                 end
             end
