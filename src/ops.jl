dc328e0eaef13e663e31ff44a1c674dc1d65639d
Jesper Stemann Andersen <jesper@sait.dk>
2025-04-06 13:07:28 +0200

    WIP

--- src/ops.jl
+++ src/ops.jl
@@ -1,3 +1,58 @@
+"""
+    dropdims(a::MLXArray; dims::Union{Dims, Integer, Nothing} = nothing)
+
+Return an array with singleton dimensions removed. If `dims` is not specified,
+all singleton dimensions are removed.
+"""
+function Base.dropdims(a::MLXArray{T, N}; dims::Union{Dims, Integer, Nothing} = nothing) where {T, N}
+    s = get_stream()
+    result = Ref(Wrapper.mlx_array_new())
+    if isnothing(dims)
+        Wrapper.mlx_squeeze_all(result, a.mlx_array, s.mlx_stream)
+    else
+        if dims isa Integer
+            dims = Dims(dims)
+        end
+        row_major_dims = ndims(a) .- dims .+ 1
+        dims = collect(Cint.(row_major_dims) .- one(Cint))
+        Wrapper.mlx_squeeze(result, a.mlx_array, dims, length(dims), s.mlx_stream)
+    end
+    remaining_dims = Int(Wrapper.mlx_array_ndim(result[]))
+    return MLXArray{T, remaining_dims}(result[])
+end
+
+function Base.sort(v::MLXVector{T}) where {T}
+    s = get_stream()
+    result = Ref(Wrapper.mlx_array_new())
+    Wrapper.mlx_sort_all(result, v.mlx_array, s.mlx_stream)
+    return MLXVector{T}(result[])
+end
+
+function Base.sort(a::MLXArray{T, N}; dims::Integer) where {T, N}
+    s = get_stream()
+    result = Ref(Wrapper.mlx_array_new())
+    row_major_dims = ndims(a) - dims + 1
+    axis = Cint(row_major_dims) - one(Cint)
+    Wrapper.mlx_sort(result, a.mlx_array, axis, s.mlx_stream)
+    return MLXArray{T, N}(result[])
+end
+
+function Base.sortperm(v::MLXVector{T}) where {T}
+    s = get_stream()
+    result = Ref(Wrapper.mlx_array_new())
+    Wrapper.mlx_argsort_all(result, v.mlx_array, s.mlx_stream)
+    return MLXVector{T}(result[])
+end
+
+function Base.sortperm(a::MLXArray{T, N}; dims::Integer) where {T, N}
+    s = get_stream()
+    result = Ref(Wrapper.mlx_array_new())
+    row_major_dims = ndims(a) - dims + 1
+    axis = Cint(row_major_dims) - one(Cint)
+    Wrapper.mlx_argsort(result, a.mlx_array, axis, s.mlx_stream)
+    return MLXArray{T, N}(result[])
+end
+
 for (fn, fn_def) in Private.get_unary_array_ops()
     TOut = fn_def.output_type(fn_def.TIn)
     kwargs_expr = Expr[]
